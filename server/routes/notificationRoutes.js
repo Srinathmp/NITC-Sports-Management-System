d7d7a55f31cdd09ae85dbd8961092e42dfdf4c8b
Srinathmp <srinathmp21@gmail.com>
2025-10-26 16:48:01 +0530

    added routes and controllers for everything except notification

--- server/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createNotification, listNotifications } = require('../controllers/notificationController');
-const { protect, authorizeRoles } = require('../middleware/authMiddleware');
-
-router.post('/', protect, authorizeRoles('CommonAdmin','NITAdmin'), createNotification);
-router.get('/', listNotifications);
-
-module.exports = router;
