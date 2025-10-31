9f2c77e9d91739c519ea7a1ef8f0f49ba10ad82d
Srinathmp <srinathmp21@gmail.com>
2025-10-31 22:14:43 +0530

    got notification route and controller code from Achut

---
+++ server/controllers/notificationController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require('express-async-handler');
+const Notification = require('../models/Notification');
+
+const createNotification = asyncHandler(async (req, res) => {
+  const { user_id, recipientRole, message, type } = req.body;
+  const note = await Notification.create({ user_id, created_by: req.user._id, recipientRole, message, type });
+  res.status(201).json(note);
+});
+
+const listNotifications = asyncHandler(async (req, res) => {
+  const { recipientRole } = req.query;
+  if (recipientRole) {
+    const list = await Notification.find({ recipientRole }).sort({ created_at: -1 });
+    return res.json(list);
+  }
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    const token = req.headers.authorization.split(' ')[1];
+    const jwt = require('jsonwebtoken');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.id;
+    const list = await Notification.find({ $or: [{ user_id: userId }, { recipientRole: 'All' }] }).sort({ created_at: -1 });
+    return res.json(list);
+  }
+  const publicList = await Notification.find({ recipientRole: 'All' }).sort({ created_at: -1 });
+  res.json(publicList);
+});
+
+module.exports = { createNotification, listNotifications };
